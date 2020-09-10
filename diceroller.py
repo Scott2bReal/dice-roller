2efa888fe36590a89ef35ca77fb6d6f546f70c61
Scott2bReal <hoeckers@gmail.com>
2020-09-10 10:09:58 -0500

    first commit

---
+++ diceroller.py
@@ -0,0 +1,42 @@
+import random
+from time import sleep
+
+while True:
+    try:
+        dice = int(input("number of dice to roll: "))
+        break
+    except ValueError:
+        print("please supply an integer")
+        continue
+
+while True:
+    try:
+        sides = int(input("number of sides: "))
+        break
+    except ValueError:
+        print("please supply an integer")
+        continue
+
+while True:
+    try:
+        bonus = int(input("bonus: "))
+        break
+    except ValueError:
+        print("please supply an integer")
+        continue
+
+sleep(.5)
+
+i = 0
+total = 0
+while i < dice:
+    roll = random.randint(1,sides + 1)
+    sleep(.5)
+    print(f'Die {i + 1}: {roll}')
+    sleep(.5)
+    total = total + roll
+    i += 1
+
+total = total + bonus
+sleep(.5)
+print(f'Final result: {total}')
